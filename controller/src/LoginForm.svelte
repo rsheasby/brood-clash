4cad0fc0679feb7fb7a82b2e5239f6c9fc6c6f98
Juan de Bruin <juandb@pm.me>
2019-10-26 14:04:58 +0200

    Add login form

---
+++ controller/src/LoginForm.svelte
@@ -0,0 +1,35 @@
+<script>
+	import {createEventDispatcher } from "svelte";
+
+	const dispatch = createEventDispatcher();
+
+	let invalidCode;
+
+	function handleSubmit(event) {
+		let loginCode = event.target.loginCode.value;
+
+		// TODO: Use login from the server.
+		if (loginCode === "secret") {
+			dispatch("login", { sessionToken: "16ha051hheef9581" });
+		} else {
+			invalidCode = true;
+		}
+	}
+</script>
+
+<style>
+	.error {
+		color: red;
+	}
+</style>
+
+<form on:submit|preventDefault="{handleSubmit}">
+	<label for="login-code-input">Please enter your login code:</label>
+	<input type="text" id="login-code-input" placeholder="Login code" name="loginCode" required><br />
+
+	<button type="submit">Login</button>
+</form>
+
+{#if invalidCode}
+<div class="error">Invalid login code!</div>
+{/if}
