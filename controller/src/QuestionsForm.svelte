f5894e83d9951cf79aeabf3b1caa4bb05782bb92
Juan de Bruin <juandb@pm.me>
2019-11-27 20:55:40 +0200

    Submit questions to server from controller

--- controller/src/QuestionsForm.svelte
+++ controller/src/QuestionsForm.svelte
@@ -1,45 +1,80 @@
 <script>
 	import { createEventDispatcher, onMount } from "svelte";
+	import page from "page";
+	import axios from "axios";
 
 	const dispatch = createEventDispatcher();
 
 	let questions = [];
+	let errors = [];
 
 	onMount(() => addQuestion());
 
 	function addQuestion() {
-		function nextId() {
-			let latestQuestion = questions[questions.length - 1];
-			if (latestQuestion) {
-				return latestQuestion.id + 1;
-			} else {
-				return 0;
-			}
-		}
-
 		let question = {
-			id: nextId(),
-			question: "",
-			answers: [
-				"", "", "", "",
-				"", "", "", ""
-			]
+			description: "",
+			answers: []
 		};
+		for (let i = 0; i < 8; ++i) {
+			question.answers.push({
+				text: "",
+				points: 0
+			});
+		}
 		questions = [...questions, question];
 	}
 
-	function handleSubmit() {
-		dispatch("submit", questions);
+	async function handleSubmit() {
+		errors = [];
+		if (questions.length < 1) {
+			errors = [...errors, "Must have at least question to submit."];
+			return;
+		}
+
+		for (let question of questions) {
+			if (question.description.length < 1) {
+				errors = [...errors, "Question cannot be an empty string."];
+				return;
+			}
+			if (question.answers.some(a => a.length < 1)) {
+				errors = [...errors, "Answers cannot be empty strings."];
+				return;
+			}
+		}
+
+		try {
+			const response = await axios({
+				url: "http://localhost:1323/api/questions",
+				method: "post",
+				headers: { "Authorization": `Bearer ${window.localStorage.getItem("code")}` },
+				data: questions
+			});
+			page.redirect("/controller");
+		} catch (e) {
+			console.log(e);
+			return;
+		}
+
 	}
 </script>
 
+<style>
+.error {
+	color: red;
+}
+</style>
+
 <form on:submit|preventDefault="{handleSubmit}">
 	{#each questions as question (question.id)}
-	Question:<br />
-	<input type="text" bind:value="{question.question}" on:keydown|stopPropagation><br />
-	Answers:<br />
+		Question:<br />
+		<input type="text" bind:value="{question.description}" on:keydown|stopPropagation>
+		<br />
+		Answers:<br />
 		{#each question.answers as answer, index}
-		{index + 1}: <input type="text" bind:value="{answer}" on:keydown|stopPropagation><br />
+			{index + 1}:
+			<input type="text" bind:value="{answer.text}" on:keydown|stopPropagation>
+			<input type="number" bind:value={answer.points} on:keydown|stopPropagation>
+			<br />
 		{/each}
 	{/each}
 
@@ -47,3 +82,7 @@
 </form>
 
 <button on:click={addQuestion} on:keydown|stopPropagation>Add question</button>
+
+{#each errors as error}
+<p class="error">{error}</p>
+{/each}
