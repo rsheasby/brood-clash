cd01af32e10f4b199c0718e3bd07b6b35812e5fa
Ryan David Sheasby <ryan@sheasby.dev>
2020-01-14 20:57:51 +0200

    Add page.js setup

--- client/src/controller/LoginForm.svelte
+++ client/src/controller/LoginForm.svelte
@@ -1,22 +1,8 @@
 <script>
-	import axios from "axios";
 	import page from "page";
 
 	let invalidCode;
 
-	async function handleSubmit(event) {
-		const code = event.target.loginCode.value;
-		try {
-			const response = await axios({
-				url: "http://localhost:1323/api/code/valid",
-				headers: { "Authorization": `Bearer ${code}` }
-			});
-			window.localStorage.setItem("code", code);
-			window.location = "/controller";
-		} catch (e) {
-			invalidCode = true;
-		}
-	}
 </script>
 
 <style>
@@ -25,12 +11,12 @@
 	}
 </style>
 
-<form on:submit|preventDefault="{handleSubmit}">
-	<label for="login-code-input">Please enter your login code:</label>
-	<input type="text" id="login-code-input" placeholder="Login code" name="loginCode" required on:keydown|stopPropagation><br />
+<!--<form on:submit|preventDefault="{handleSubmit}">-->
+<!--	<label for="login-code-input">Please enter your login code:</label>-->
+<!--	<input type="text" id="login-code-input" placeholder="Login code" name="loginCode" required on:keydown|stopPropagation><br />-->
 
-	<button type="submit" on:keydown|stopPropagation>Login</button>
-</form>
+<!--	<button type="submit" on:keydown|stopPropagation>Login</button>-->
+<!--</form>-->
 
 {#if invalidCode}
 <div class="error">Invalid login code!</div>
