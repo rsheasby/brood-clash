402f85b7aaafcd2d20608419c392b6b1ca5b7985
Juan de Bruin <juan@debruin.dev>
2020-01-16 19:30:15 +0200

    Remove old code

--- client/src/controller/LoginForm.svelte
+++ client/src/controller/LoginForm.svelte
@@ -1,23 +1,2 @@
 <script>
-	import page from "page";
-
-	let invalidCode;
-
 </script>
-
-<style>
-	.error {
-		color: red;
-	}
-</style>
-
-<!--<form on:submit|preventDefault="{handleSubmit}">-->
-<!--	<label for="login-code-input">Please enter your login code:</label>-->
-<!--	<input type="text" id="login-code-input" placeholder="Login code" name="loginCode" required on:keydown|stopPropagation><br />-->
-
-<!--	<button type="submit" on:keydown|stopPropagation>Login</button>-->
-<!--</form>-->
-
-{#if invalidCode}
-<div class="error">Invalid login code!</div>
-{/if}
--- client/src/controller/PleaseWait.svelte
+++
@@ -1,2 +0,0 @@
-<p>Loading application, please wait.</p>
-<p>This should not take long. If it takes more than 10 second, please reload the page.</p>
