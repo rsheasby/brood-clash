03730a83d0e7179c084030f9d38c227b60173322
Juan de Bruin <juan@debruin.dev>
2020-01-21 14:33:27 +0200

    Add temporary CORS middleware since Ryan's "solution" didn't work

--- backend/restapi/configure_brood_clash.go
+++ backend/restapi/configure_brood_clash.go
@@ -81,10 +81,5 @@ func setupMiddlewares(handler http.Handler) http.Handler {
 // The middleware configuration happens before anything, this middleware also applies to serving the swagger.json document.
 // So this is a good place to plug in a panic handling middleware, logging and metrics
 func setupGlobalMiddleware(handler http.Handler) http.Handler {
-	handleCORS := cors.New(cors.Options{
-		AllowedOrigins:         []string{"*"},
-		AllowCredentials:       true,
-	})
-
-	return handleCORS.Handler(handler)
+	return cors.AllowAll().Handler(handler)
 }
