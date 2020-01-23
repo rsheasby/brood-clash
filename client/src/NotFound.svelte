9858ad518991ccd262bb5791a60e55c4e6ed96e7
Juan de Bruin <juan@debruin.dev>
2020-01-23 15:42:00 +0200

    Add routing options on page not found page

--- client/src/NotFound.svelte
+++ client/src/NotFound.svelte
@@ -1,1 +1,10 @@
 <p>Couldn't find page "{window.location.pathname}".</p>
+
+<ul>
+    <li>
+        <a href="/controller/login">Controller</a>
+    </li>
+    <li>
+        <a href="/presenter">Presenter</a>
+    </li>
+</ul>
