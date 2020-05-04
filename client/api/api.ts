c1725e7ad3b728eb5daebccfc3d2b6ef217a81a8
Ryan David Sheasby <ryan@sheasby.dev>
2020-05-05 00:35:39 +0200

    Add axios interceptor for auto redirect

--- client/api/api.ts
+++ client/api/api.ts
@@ -1,9 +1,16 @@
 import { DefaultApi, Configuration } from './codegen/index';
+import * as axios from 'axios';
 
 export let ApiClient = new DefaultApi(new Configuration({
 	apiKey: "",
 	basePath: `http://${location.hostname}:8080/api/v1`
 }));
+
+axios.default.interceptors.response.use(config => config, error => {
+	if (location.pathname != "/controller/login")
+		window.history.replaceState({}, "", "/controller/login");
+	return error;
+});
 
 export function SetApiKey(key: string) {
 	ApiClient = new DefaultApi(new Configuration({
