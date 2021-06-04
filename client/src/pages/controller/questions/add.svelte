c7380aaa3c934522f8803f8d20be148b1e94d9ce
Ryan David Sheasby <ryan@sheasby.dev>
2021-06-04 12:46:38 +0200

    Add check for empty question

--- client/src/pages/controller/questions/add.svelte
+++ client/src/pages/controller/questions/add.svelte
@@ -8,15 +8,15 @@
 
 	function newQuestion(): codegen.ModelsQuestion {
 		return {
-			answers: [ newAnswer() ],
-			text: ""
+			answers: [newAnswer()],
+			text: '',
 		};
 	}
 
 	function newAnswer(): codegen.ModelsAnswer {
 		return {
 			points: undefined,
-			text: ""
+			text: '',
 		};
 	}
 
@@ -25,8 +25,9 @@
 	 */
 	async function submitQuestion(): Promise<boolean> {
 		attemptedSubmit = true;
+		if (question.text === '') return false;
 		try {
-			const response = await ApiClient.postQuestions([ question ]);
+			const response = await ApiClient.postQuestions([question]);
 			const status = response && response.status;
 			return status === 201;
 		} catch (e) {
@@ -55,7 +56,7 @@
 
 	async function addAnotherAnswer() {
 		attemptedSubmit = false;
-		question.answers = [ ...question.answers, newAnswer() ]
+		question.answers = [...question.answers, newAnswer()];
 	}
 </script>
 
@@ -75,19 +76,37 @@
 	<div class="card grid grid-cols-1 gap-3">
 		<form id="questions-form" class="grid grid-cols-1 gap-3">
 			<input
-				class="input box-content w-auto max-w-full border-4" type="text"
-				required placeholder="Question"
-				bind:value={question.text} />
+				class="input box-content w-auto max-w-full border-4 {question.text ===
+					'' && attemptedSubmit
+					? 'border-red-500 border-4'
+					: ''}"
+				type="text"
+				required
+				placeholder="Question"
+				bind:value={question.text}
+			/>
 			{#each question.answers as answer, i}
 				<div class="grid grid-cols-4 gap-3">
-					<input class="input col-span-3 w-auto
-									  max-w-full {answer.text === '' && attemptedSubmit ? 'border-red-500 border-4 -m-2px' : ''}" type="text" required
+					<input
+						class="input col-span-3 w-auto
+									  max-w-full {answer.text === '' && attemptedSubmit
+							? 'border-red-500 border-4 -m-2px'
+							: ''}"
+						type="text"
+						required
 						placeholder="Answer {i + 1}"
-						bind:value={answer.text}/>
-					<input class="input col-span-1 w-auto
-						max-w-full {answer.points === undefined && attemptedSubmit ? 'border-red-500 border-4 -m-2px' : ''}" type="number" required
+						bind:value={answer.text}
+					/>
+					<input
+						class="input col-span-1 w-auto
+						max-w-full {answer.points === undefined && attemptedSubmit
+							? 'border-red-500 border-4 -m-2px'
+							: ''}"
+						type="number"
+						required
 						placeholder="Points"
-						bind:value={answer.points} />
+						bind:value={answer.points}
+					/>
 				</div>
 			{/each}
 			{#if question.answers.length < 8}
@@ -99,25 +118,33 @@
 					will defocus once you make that pseudo box a real box and I don't
 					want to deal with the complexities of that right now.
 				-->
-				<button class="button-neutral box-content w-auto w-max-full"
-					on:click={() => addAnotherAnswer()}>
+				<button
+					class="button-neutral box-content w-auto w-max-full"
+					on:click={() => addAnotherAnswer()}
+				>
 					Add another answer
 				</button>
 			{/if}
 		</form>
 		<div class="grid grid-cols-3 gap-3">
-			<button class="button-warning box-content w-auto w-max-full"
-				on:click={() => $goto('/controller/questions')}>
+			<button
+				class="button-warning box-content w-auto w-max-full"
+				on:click={() => $goto('/controller/questions')}
+			>
 				Cancel
 			</button>
 
-			<button class="button-primary box-content w-auto w-max-full"
-				on:click={submitAndAddAnother}>
+			<button
+				class="button-primary box-content w-auto w-max-full"
+				on:click={submitAndAddAnother}
+			>
 				Submit and add another
 			</button>
 
-			<button class="button-primary box-content w-auto w-max-full"
-				on:click={submitAndBackToQuestions}>
+			<button
+				class="button-primary box-content w-auto w-max-full"
+				on:click={submitAndBackToQuestions}
+			>
 				Submit
 			</button>
 		</div>
